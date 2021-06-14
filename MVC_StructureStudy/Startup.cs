30244b0c39fed1d07085d35cb1a86c4f6571acf9
Ariel <kagari0803@gmail.com>
2021-06-14 16:28:39 +0800

    add comments to 47:10

--- MVC_StructureStudy/Startup.cs
+++ MVC_StructureStudy/Startup.cs
@@ -21,35 +21,45 @@ namespace MVC_StructureStudy
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //register the service here
         public void ConfigureServices(IServiceCollection services)
         {
+            //register MVC to this project
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //IWebHostEnvironment env is to check if now is in the development environment
             if (env.IsDevelopment())
             {
+                //developer error message
                 app.UseDeveloperExceptionPage();
             }
             else
             {
+                //error message to user
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            //force the user use secure line
             app.UseHttpsRedirection();
+            //can use static files in wwwroot(ex:html/css/js)
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
+            //last step
             app.UseEndpoints(endpoints =>
             {
+                //endpoints about MVC (can add more like razor)
                 endpoints.MapControllerRoute(
                     name: "default",
+                    //default controller name=home action=Index id is optional
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
